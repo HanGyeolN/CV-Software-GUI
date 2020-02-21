1276853ec28d548e52a31cae1623d4bc34ea6c0d
HanGyeolN <ghnruf@naver.com>
2020-02-21 13:25:12 +0900

    Add | cv_basic.py Image Masking

--- 190627_Project.py
+++ 190627_Project.py
@@ -91,66 +91,12 @@ class MyWindow(QMainWindow, form_class):
         self.displayOutputImage()
 
     def binary(self):
-        Threshold = np.mean(self.input_array)
-        nRow = self.input_image.height()
-        nCol = self.input_image.width()
-        self.output_array = self.input_array.copy()
-        
-        self.output_array[0] = (self.output_array[0] + self.output_array[1] + self.output_array[2]) / 3
-        self.output_array[0] = np.where(self.output_array[0] > Threshold, 255, 0)
-        self.output_array[1] = self.output_array[0].copy()
-        self.output_array[2] = self.output_array[0].copy()
+        cv_basic.binarize_image(self)
+        self.displayOutputImage()
 
-        self.displayOutputImage(nRow,nCol)
-
-####### 화소 영역 처리
     def masking(self):
-        start = time.time()
-
-    # 1. 변수선언 및 메모리 할당 - 변경할 크기
-        nRow = self.input_image.height()
-        nCol = self.input_image.width()
-        
-        maskSelector = QInputDialog.getText(self, "마스크 선택","1. 엠보싱 2. 블러링 3. 샤프닝 4. 가우시안 5. 커스텀")
-        maskSelector = int(maskSelector[0])
-
-        if maskSelector == 1: # 엠보싱
-            mask = np.array([[-1, 0, 0], [ 0, 0, 0], [ 0, 0, 1]])
-        elif maskSelector == 2: # LPF(블러링)
-            mask = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
-        elif maskSelector == 3: # HPF(샤프닝)
-            mask = np.array([[-1/9, -1/9, -1/9], [ -1/9, 8/9, -1/9], [ -1/9, -1/9, -1/9]])
-        elif maskSelector == 4: # 가우시안 필터
-            mask = np.array([[1/16, 1/8, 1/16], [1/8, 1/4, 1/8], [1/16, 1/8, 1/16]])
-        elif maskSelector == 5: # Custom
-            mask = QInputDialog.getText(self, "마스크 입력","3x3 : 9개 입력. \n,로 구분")
-            mask = mask[0]
-            mask = mask.split(',')
-            for i in range(len(mask)):
-                mask[i] = int(mask[i])
-            mask = np.array(mask)
-            mask = mask.reshape((3,3))
-
-        self.output_array = np.zeros((3,nRow,nCol))
-    
-    # 2. 알고리즘 - 마스킹
-        if maskSelector == 1: # 엠보싱
-            outputHsvArray = matplotlib.colors.rgb_to_hsv(self.input_array.transpose(1, 2, 0))
-            outputHsvArray[:,:,2] = ndimage.convolve(outputHsvArray[:,:,2], mask)
-
-            self.output_array = matplotlib.colors.hsv_to_rgb(outputHsvArray).copy()
-            self.output_array = self.output_array.transpose(2,0,1) + 127
-
-        else:
-            for RGB in range(3):
-                self.output_array[RGB] = ndimage.convolve(self.input_array[RGB], mask)
-
-        self.output_array = np.where(self.output_array > 255, 255, np.where(self.output_array < 0 , 0, self.output_array))
-        #self.output_array = self.output_array.astype(np.uint8)
-
-
-        
-        self.displayOutputImage(nRow,nCol)
+        cv_basic.masking_image(self)
+        self.displayOutputImage()
 
 ####### 기하처리
     def resizing(self):
--- srcs/cv_basic.py
+++ srcs/cv_basic.py
@@ -1,10 +1,62 @@
 from PyQt5.QtWidgets import QInputDialog
+from scipy import ndimage
+import matplotlib
 import numpy as np
 
 def brightness_control(self):
+    """
+    Control image brightness.
+    """
     # 얼마나 조절할지 입력
     intValue = QInputDialog.getInt(self,"test", "brightness(-255~255)")
     self.output_array = self.input_array + intValue[0]
 
     self.output_array = np.where(self.output_array > 255, 255,
         np.where(self.output_array < 0 , 0, self.output_array))
+
+def binarize_image(self):
+    """
+    Binarize image.
+    """
+    # Threshold = 평균값
+    Threshold = np.mean(self.input_array)
+    self.output_array = self.input_array.copy()
+    
+    self.output_array[0] = (self.output_array[0] + self.output_array[1] + self.output_array[2]) / 3
+    self.output_array[0] = np.where(self.output_array[0] > Threshold, 255, 0)
+    self.output_array[1] = self.output_array[0].copy()
+    self.output_array[2] = self.output_array[0].copy()
+
+def masking_image(self):
+    nRow = self.input_image.height()
+    nCol = self.input_image.width()
+    self.output_array = np.zeros((3,nRow,nCol))
+    maskSelector = QInputDialog.getText(self, "마스크 선택","1. 엠보싱 2. 블러링 3. 샤프닝 4. 가우시안 5. 커스텀")
+    maskSelector = int(maskSelector[0])
+
+    if maskSelector == 1: # 엠보싱
+        mask = np.array([[-1, 0, 0], [ 0, 0, 0], [ 0, 0, 1]])
+    elif maskSelector == 2: # LPF(블러링)
+        mask = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
+    elif maskSelector == 3: # HPF(샤프닝)
+        mask = np.array([[-1/9, -1/9, -1/9], [ -1/9, 8/9, -1/9], [ -1/9, -1/9, -1/9]])
+    elif maskSelector == 4: # 가우시안 필터
+        mask = np.array([[1/16, 1/8, 1/16], [1/8, 1/4, 1/8], [1/16, 1/8, 1/16]])
+    elif maskSelector == 5: # Custom
+        mask = QInputDialog.getText(self, "마스크 입력","3x3 : 9개 입력. \n,로 구분")
+        mask = mask[0]
+        mask = mask.split(',')
+        for i in range(len(mask)):
+            mask[i] = int(mask[i])
+        mask = np.array(mask)
+        mask = mask.reshape((3,3))
+
+    if maskSelector == 1: # 엠보싱
+        outputHsvArray = matplotlib.colors.rgb_to_hsv(self.input_array.transpose(1, 2, 0))
+        outputHsvArray[:,:,2] = ndimage.convolve(outputHsvArray[:,:,2], mask)
+        self.output_array = matplotlib.colors.hsv_to_rgb(outputHsvArray).copy()
+        self.output_array = self.output_array.transpose(2,0,1) + 127
+    else:
+        for RGB in range(3):
+            self.output_array[RGB] = ndimage.convolve(self.input_array[RGB], mask)
+    self.output_array = np.where(self.output_array > 255, 255, np.where(self.output_array < 0 , 0, self.output_array))
